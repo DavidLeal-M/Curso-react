1ab8d76ff8e319b069bbae7d302f0ceff2d337eb
David <davidlealmonti@gmail.com>
2022-01-20 16:17:08 -0600

    first commit

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,9 @@
+
+//Variables y constantes
+
+const nombre = 'David';
+const apellido = 'Leal';
+
+let valorDado=5;
+valorDado=4 ;
+console.log(nombre,apellido,valorDado)
---
+++ src/bases/03-objeto-literal.js
@@ -0,0 +1,20 @@
+
+
+const persona ={
+    nombre: 'Tony',
+    apellido: 'Stark',
+    edad: 45 ,
+    direccion :{
+        ciudad: 'New York',
+        Zip:5535353,
+        lat:14.656,
+        lng:36.99898
+    }
+};
+
+ //console.table(persona);
+ console.log(persona);
+
+ const persona2 = {...persona};
+ console.log(persona2);
+
